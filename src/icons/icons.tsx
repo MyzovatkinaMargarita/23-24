034002fee742881ea26aeca3cf94fea706761b63
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-12-16 20:32:57 +0300

    icons.tsx

---
+++ src/icons/icons.tsx
@@ -0,0 +1,8 @@
+
+export function DoneIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 12L10.2426 16.2426L18.727 7.75732" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  );
+}
